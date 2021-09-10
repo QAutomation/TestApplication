87417c38e0d631b07e603458f624094cf1ebe6fb
Viktor Khodzinskyi <Viktor.Khodzinskyi@tx.local>
2021-09-10 12:12:03 +0300

    grades controller test added

---
+++ api.Tests/Controllers/GradesApiTests.cs
@@ -0,0 +1,22 @@
+﻿using api.Controllers;
+using api.Tests.Model;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MongoDB.Driver;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+
+namespace api.Tests.Controllers
+{
+    [TestClass]
+    public class GradesApiTests
+    {
+        [TestMethod]
+        public void Get()
+        {
+            GradesController controller = new GradesController();
+            IEnumerable<GradesDbModel.ApiModel> items = controller.Get(1, 10);
+            Assert.AreEqual(items.Count(), 10);
+        }
+    }
+}
